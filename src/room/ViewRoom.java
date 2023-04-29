a32e2682cdc78642832a085119ab379d716f3485
alexanderis00 <82192504+alexanderis00@users.noreply.github.com>
2023-04-29 23:40:43 +0700

    u

--- src/room/ViewRoom.java
+++ src/room/ViewRoom.java
@@ -1,9 +1,6 @@
 package room;
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,15 +9,12 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class ViewRoom extends JFrame implements ActionListener{
